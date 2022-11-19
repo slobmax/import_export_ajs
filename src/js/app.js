eb7597ec2bad6bbf189ccce0f78e3990fcc2e85d
Maxim Slobozhanin <slob.max@gmail.com>
2022-11-19 15:53:00 +0400

    task 2 completed

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+const game = new Game();
+game.start();
+
+import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game.js';
+console.log(Game, GameSavingData, loadGame, saveGame);
---
+++ src/js/game.js
@@ -0,0 +1,17 @@
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+  };
+  
+export class GameSavingData {
+};
+  
+export function readGameSaving() {
+};
+  
+export function writeGameSaving() {
+};
+
+import Character from './domain.js';
+console.log(Character);
